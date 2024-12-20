8cd5a236e06ae95b48c420853bc55a974a01a97e
satyammankar01 <satyam.mankar01@gmail.com>
2024-12-20 14:35:39 +0530

    Add files via upload

---
+++ server/globalData/index.js
@@ -0,0 +1,9 @@
+module.exports.MAIL_TEMPLATE_TYPE = {
+  Welcome: "Welcome",
+  KickStudent: "Kick Student",
+  DeleteCourse: "Delete Course",
+  WithdrawCourse: "Withdraw Course",
+  CreateAttendance: "Create Attendance",
+};
+
+module.exports.OfficialURL = "https://VisiMark.netlify.app";
